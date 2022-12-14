446fa257c4a3633ad34fe122f7da2562bdb6e846
khanh <kenkukhanh@gmail.com>
2022-12-15 00:29:17 +0700

    update file

--- common.sh
+++ common.sh
@@ -4,14 +4,14 @@ set -ex
 # Turn on export DEBIAN_FRONTEND=noninteractive if docker run mysql
 #export DEBIAN_FRONTEND=noninteractive
 
-apt update
+sudo apt update
 
-apt install -y systemd systemd-sysv kmod coreutils lsb-release wget curl zip unzip tar busybox iputils-ping iproute2 net-tools jq gnupg2 netcat bind9-dnsutils openssh-client git binutils ripgrep bash-completion
+sudo apt install -y systemd systemd-sysv kmod coreutils lsb-release wget curl zip unzip tar busybox iputils-ping iproute2 net-tools jq gnupg2 netcat bind9-dnsutils openssh-client git binutils ripgrep bash-completion
 
-apt install -y mariadb-server
+sudo apt install -y mariadb-server
 
-curl https://getmic.ro | bash && mv micro /usr/local/bin
+#curl https://getmic.ro | bash && mv micro /usr/local/bin
 
-sed -i -E 's/^(bind-address += )([0-9].+)$/\1 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo sed -i -E 's/^(bind-address += )([0-9].+)$/\1 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-sed -i -E "s/^#(log_bin +=)(.*$)/\1\2/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo sed -i -E "s/^#(log_bin +=)(.*$)/\1\2/g" /etc/mysql/mariadb.conf.d/50-server.cnf
--- scripts/master.sh
+++ scripts/master.sh
@@ -3,9 +3,10 @@ set -ex
 IP_MASTER=${IP_MASTER:-}
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-root}
 MYSQL_SLAVE_PASSWORD=${MYSQL_SLAVE_PASSWORD:-slave}
-ENDIP=$(ip addr show eth0 | grep 'inet ' | sed -E 's/\s+inet ([0-9.]+)\/[0-9]+ .*/\1/g' | head -n 1 | sed -E 's/^([0-9].+)([{.}])([0-9]+)$/\3/g')
+NAME_NET=$(ip route list | grep 'default' | grep -Eo 'dev [a-z0-9]+' | awk '{print $2}')
+ENDIP=$(ip addr show $NAME_NET | grep 'inet ' | sed -E 's/\s+inet ([0-9.]+)\/[0-9]+ .*/\1/g' | head -n 1 | sed -E 's/^([0-9].+)([{.}])([0-9]+)$/\3/g')
 # If use docker run mysql no need use service mysql start
-#service mysql start
+sudo service mysql start
 
 while :; 
 do
@@ -17,13 +18,14 @@ do
   fi
 done
 
-echo "CREATE USER root@\"%\" IDENTIFIED BY \"$MYSQL_ROOT_PASSWORD\"" | mysql
+echo "CREATE USER root@\"%\" IDENTIFIED BY \"$MYSQL_ROOT_PASSWORD\"" | sudo mysql
 
-if [ -z $(echo 'select Host from mysql.user where User="slave"' | mysql) ]; then
-  echo "CREATE USER \"slave\"@\"%\" IDENTIFIED BY \"$MYSQL_SLAVE_PASSWORD\"" | mysql
-  echo "GRANT REPLICATION SLAVE ON *.* TO \"slave\"@\"%\"" | mysql
+if [ -z $(echo 'select Host from mysql.user where User="slave"' | sudo mysql) ]; then
+  echo "CREATE USER \"slave\"@\"%\" IDENTIFIED BY \"$MYSQL_SLAVE_PASSWORD\"" | sudo mysql
+  echo "GRANT REPLICATION SLAVE ON *.* TO \"slave\"@\"%\"" | sudo mysql
+  echo "FLUSH PRIVILEGES;" | sudo mysql
 fi
 
-sed -i -E "s/^#server-id += [0-9]$/server-id           =$ENDIP/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sudo sed -i -E "s/^#server-id += [0-9]$/server-id           =$ENDIP/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-service mysql restart
+sudo service mysql restart
