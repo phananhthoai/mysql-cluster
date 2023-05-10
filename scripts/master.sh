bc0cdea811ee74ff5323ba0377850ac44fd870a8
phananhthoai <106568523+phananhthoai@users.noreply.github.com>
2023-05-10 14:28:56 +0700

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 set -ex
+
+if [ $(id -u) != 0 ]; then
+  sudo ${0}
+  exit 0
+fi
+
 IP_MASTER=${IP_MASTER:-}
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-root}
 MYSQL_SLAVE_PASSWORD=${MYSQL_SLAVE_PASSWORD:-slave}
 NAME_NET=$(ip route list | grep 'default' | grep -Eo 'dev [a-z0-9]+' | awk '{print $2}')
 ENDIP=$(ip addr show $NAME_NET | grep 'inet ' | sed -E 's/\s+inet ([0-9.]+)\/[0-9]+ .*/\1/g' | head -n 1 | sed -E 's/^([0-9].+)([{.}])([0-9]+)$/\3/g')
 # If use docker run mysql no need use service mysql start
-sudo service mysql start
+service mysql start
 
 while :; 
 do
@@ -18,16 +24,16 @@ do
   fi
 done
 
-echo "CREATE USER root@\"%\" IDENTIFIED BY \"$MYSQL_ROOT_PASSWORD\"" | sudo mysql
-echo "GRANT ALL PRIVILEGES ON *.* TO \"root\"@\"%\"" | sudo mysql;
-echo "FLUSH PRIVILEGES;" | sudo mysql
+echo "CREATE USER root@\"%\" IDENTIFIED BY \"$MYSQL_ROOT_PASSWORD\"" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO \"root\"@\"%\"" | mysql;
+echo "FLUSH PRIVILEGES;" | mysql
 
-if [ -z $(echo 'select Host from mysql.user where User="slave"' | sudo mysql) ]; then
-  echo "CREATE USER \"slave\"@\"%\" IDENTIFIED BY \"$MYSQL_SLAVE_PASSWORD\"" | sudo mysql
-  echo "GRANT REPLICATION SLAVE ON *.* TO \"slave\"@\"%\"" | sudo mysql
-  echo "FLUSH PRIVILEGES;" | sudo mysql
+if [ -z $(echo 'select Host from mysql.user where User="slave"' | mysql) ]; then
+  echo "CREATE USER \"slave\"@\"%\" IDENTIFIED BY \"$MYSQL_SLAVE_PASSWORD\"" | mysql
+  echo "GRANT REPLICATION SLAVE ON *.* TO \"slave\"@\"%\"" | mysql
+  echo "FLUSH PRIVILEGES;" | mysql
 fi
 
-sudo sed -i -E "s/^#server-id += [0-9]$/server-id           =$ENDIP/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -E "s/^#server-id += [0-9]$/server-id           =$ENDIP/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-sudo service mysql restart
+service mysql restart
